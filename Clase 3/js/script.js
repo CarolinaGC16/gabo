f17b75c5c2cd27e9fabe1043d59f326bb651b0a2
carolina160615 <caritoceball16@hotmail.comgit config --global user.email caritoceball16@hotmail.com>
2021-02-23 13:50:21 -0500

    Primer Commit

---
+++ Clase 3/js/script.js
@@ -0,0 +1,5 @@
+function LibroGabo() {
+    
+    alert("EL Amor En Los Tiempos Del Colera Género Novela romántica Ambientada en los años 1910, Idioma Español, Editorial Penguin Random House, País Colombia,Fecha de publicación 1985, Páginas 416");
+    
+}
